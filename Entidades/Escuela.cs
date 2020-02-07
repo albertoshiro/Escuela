0dfd4059673f02b13c009b1e506353348674e714
Alberto Monterrubio <alberto.toshiro@gmail.com>
2020-02-07 12:23:50 -0600

    commit hechovel 07-02-2020 al oarecer con el tema de arreglos finalizados

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -10,13 +10,21 @@ namespace CoreEscuela.Entidades
         public  int  AñoDeCreacion{ get;set;}
         public string Pais { get; set; }
         public string Ciudad { get; set; }
+        public Curso[] MyProperty { get; set; }
 
         public  TiposEscuela TipoEscuela{get; set;}
-        
+
+        public Curso[] Cursos   {get ; set ;}
         public Escuela (string nombre, int año) => (Nombre, AñoDeCreacion) =(nombre,año);
 
+        public Escuela(string nombre,int año, TiposEscuela tipo, string pais = "",string ciudad = ""){
+            (Nombre,AñoDeCreacion) = (nombre,año);
+            TipoEscuela = tipo;
+            Pais = pais;
+            Ciudad = ciudad;
+        }
         public override  string ToString(){
-            return $"Nombre : {Nombre}, Tipo :{TipoEscuela} \n Pais :{Pais}, Ciudad :{Ciudad}";
+            return $"Nombre : \"{Nombre} \", Tipo :{TipoEscuela} {System.Environment.NewLine}  Pais :{Pais}, Ciudad :{Ciudad}";
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,17 +1,54 @@
 ﻿using System;
 using CoreEscuela.Entidades;
+using static System.Console;
 
 namespace Etapa1
 {
     class Program
     {
         static void Main(string[] args)
-        {   
-            var escuela = new Escuela("Platzi academy",2012);
-            escuela.Pais = "Colombia";
-            escuela.Pais = "Bogota";
-            escuela.TipoEscuela = TiposEscuela.Primaria;
+        {
+            var escuela = new Escuela("Platzi academy", 2012, TiposEscuela.Primaria, pais: "Colombia", ciudad: "Bogota");
+            //escuela.Pais = "Colombia";
+            //escuela.Pais = "Bogota";
+
+            escuela.Cursos =new Curso[] {
+                new Curso(){Nombre = "101"},
+                new Curso(){Nombre= "202"},
+                new Curso(){Nombre= "303"},
+            };
+
             Console.WriteLine(escuela);
+            System.Console.WriteLine("========================================================");
+            ImprimirCursosEscuela(escuela);
+
+           
+
+
+        }
+
+        private static void ImprimirCursosEscuela(Escuela escuela)
+        {
+            WriteLine("==============================");
+            WriteLine("Cursos de la Escuela : ");
+            WriteLine("==============================");
+            
+            if (escuela !=null && escuela.Cursos != null)
+            {  
+                foreach(var curso in escuela.Cursos)
+                {
+                    System.Console.WriteLine($"Nombre : {curso.Nombre} , ID; {curso.UniqueId} \n");
+                }
+            }
+            
+        }
+
+        private static void ImprimirCursosForEach(Curso[] arregloCursos)
+        {
+           foreach (var curso in arregloCursos)
+           {
+               System.Console.WriteLine($"Nombre : {curso.Nombre} , ID; {curso.UniqueId} \n");
+           }
         }
     }
 }
